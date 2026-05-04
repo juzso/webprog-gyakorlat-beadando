ada0b77e9ac6a89f3f7b2f1c05943567c480f88d
juzso <tyityo123@gmail.com>
2026-05-04 22:42:46 +0200

    config átírva, sql csatlakozás hozzáadva

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,31 +1,35 @@
 <?php
 $ablakcim = array(
-    'cim' => 'Mini honlap Kft.',
-);
-
+    'cim' => 'Mozi Portál'
+    );
 $fejlec = array(
     'kepforras' => 'logo.png',
-    'kepalt' => 'logo',
-	'cim' => 'Mini honlap',
-	'motto' => ''
-);
-
+    'kepalt' => 'Mozi logó',
+    'cim' => 'Mozi Portál',
+    'motto' => 'Magyar filmek és mozik adatbázisa.'
+    );
 $lablec = array(
-    'copyright' => 'Copyright '.date("Y").'.',
-    'ceg' => 'Mini honlap Kft.'
-);
+    'copyright' => 'Copyright Ács András & Csányi Kristóf ',
+    'ceg' => ''
+    );
 
 $oldalak = array(
-	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menun' => array(1,1)),
-	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menun' => array(1,1)),
-	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
-	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Cikkek', 'menun' => array(1,1)),
-    'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menun' => array(1,1)),
+    '/' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+    'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
+    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)),
+    'crud' => array('fajl' => 'crud', 'szoveg' => 'CRUD', 'menun' => array(1,1)),
     'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
     'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
     'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
     'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
 );
+$hiba_oldal = array('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
 
-$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+function db_connect() {
+    $dbh = new PDO('mysql:host=localhost;dbname=', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+    $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
+    return $dbh;
+}
+function h($s) { return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8'); }
 ?>
