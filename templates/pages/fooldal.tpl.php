84992f88cb2e524639c4390fdd34f395f765c608
juzso <tyityo123@gmail.com>
2026-05-06 21:24:40 +0200

    főoldal hozzáadva

---
+++ templates/pages/fooldal.tpl.php
@@ -0,0 +1,48 @@
+<section class="hero">
+    <div>
+        <h2>Üdv a Mozi Portálon!</h2>
+
+        <p>
+            A weboldal a feltöltött mozi adatbázisra épül: klasszikus magyar filmek,
+            mozik és előadások adatai jelennek meg benne.
+        </p>
+
+        <p>
+            <a class="btn" href="?crud">CRUD megnyitása</a>
+        </p>
+    </div>
+</section>
+
+<section class="ket-oszlop">
+    <article>
+        <h3>Saját videó</h3>
+
+        <div class="video-box">
+            <video controls>
+                <source src="./videos/intro.mp4" type="video/mp4">
+                A böngésző nem támogatja a videót.
+            </video>
+        </div>
+    </article>
+
+    <article>
+        <h3>YouTube videó</h3>
+
+        <div class="youtube-box">
+            <iframe
+                src="https://www.youtube.com/embed/UGp1Lv333BQ"
+                title="YouTube video"
+                allowfullscreen>
+            </iframe>
+        </div>
+    </article>
+</section>
+
+<section>
+    <h3>Térkép</h3>
+    <iframe
+        class="map"
+        src="https://www.google.com/maps?q=Ur%C3%A1nia%20Nemzeti%20Filmsz%C3%ADnh%C3%A1z%20Budapest&output=embed"
+        loading="lazy">
+    </iframe>
+</section>
